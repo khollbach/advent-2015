0e4e7b49208f84db720800e5ff0d5bfab1a1514c
Kevan Hollbach <khollbach@gmail.com>
2024-05-11 09:15:33 -0400

    15-1

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,13 +1,8 @@
 use anyhow::Result;
 use std::{cmp::max, iter::zip};
 
-/*
-attemps:
-* 225000000 -- too high
-
-*/
-
-fn main() -> Result<()> {
+// 225000000 -- too high
+fn _attempt_1() -> Result<()> {
     let inputs = [[3, 0, 0, -3], [-3, 3, 0, 0], [-1, 0, 4, 0], [0, 0, -2, 2]];
 
     let mut best = 0;
@@ -29,6 +24,44 @@ fn main() -> Result<()> {
                     }
                     best = max(best, score);
                 }
+            }
+        }
+    }
+
+    println!("{best}");
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let inputs = [[3, 0, 0, -3], [-3, 3, 0, 0], [-1, 0, 4, 0], [0, 0, -2, 2]];
+
+    let mut best = 0;
+
+    for a in 0..=100 {
+        for b in 0..=100 - a {
+            for c in 0..=100 - a - b {
+                let d = 100 - a - b - c;
+
+                let mut props = [0; 4];
+                for (ingredient, amount) in zip(inputs, [a, b, c, d]) {
+                    for i in 0..4 {
+                        props[i] += ingredient[i] * amount;
+                    }
+                }
+
+                // oops! we needed this.
+                for i in 0..4 {
+                    if props[i] < 0 {
+                        props[i] = 0;
+                    }
+                }
+
+                let mut score = 1;
+                for i in 0..4 {
+                    score *= props[i];
+                }
+                best = max(best, score);
             }
         }
     }
