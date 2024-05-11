df404ae82e91f3693c587ef63d047513e66dc02f
Kevan Hollbach <khollbach@gmail.com>
2024-05-11 10:04:18 -0400

    16-2

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -50,7 +50,8 @@ fn parse_aunt(line: &str) -> Result<HashMap<String, u32>> {
 
 const ACTUAL: &str = "Sue 0: children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1";
 
-fn main() -> Result<()> {
+#[allow(unused)]
+fn part_1() -> Result<()> {
     let actual = parse_aunt(ACTUAL)?;
     for (aunt, i) in zip(read_input()?, 1..) {
         if is_subset(&aunt, &actual) {
@@ -64,4 +65,29 @@ fn main() -> Result<()> {
 /// We'll say `x subset y` if every kv pair in x is also in y.
 fn is_subset(x: &HashMap<String, u32>, y: &HashMap<String, u32>) -> bool {
     x.iter().all(|(k, v)| y.get(k) == Some(v))
+}
+
+fn main() -> Result<()> {
+    let actual = parse_aunt(ACTUAL)?;
+    for (aunt, i) in zip(read_input()?, 1..) {
+        if is_possible(&aunt, &actual) {
+            println!("{i}");
+        }
+    }
+
+    Ok(())
+}
+
+/// See the rules for part two.
+fn is_possible(candidate: &HashMap<String, u32>, actual: &HashMap<String, u32>) -> bool {
+    candidate.iter().all(|(k, &v)| {
+        if !actual.contains_key(k) {
+            return false;
+        }
+        match k.as_str() {
+            "cats" | "trees" => v > actual[k],
+            "pomeranians" | "goldfish" => v < actual[k],
+            _ => v == actual[k],
+        }
+    })
 }
