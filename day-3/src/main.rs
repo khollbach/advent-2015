adaf0e3a18e9c7084407598ba5bfdc65d301417d
Kevan Hollbach <khollbach@gmail.com>
2024-05-11 08:40:55 -0400

    3-2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -2,7 +2,7 @@ use anyhow::{bail, Context, Result};
 use itertools::Itertools;
 use std::{collections::HashSet, io};
 
-fn main() -> Result<()> {
+fn _main() -> Result<()> {
     let (input,) = io::stdin()
         .lines()
         .collect_tuple()
@@ -31,3 +31,58 @@ fn main() -> Result<()> {
 
     Ok(())
 }
+
+// ---
+
+fn traverse(directions: &str, visited: &mut HashSet<(i32, i32)>) -> Result<()> {
+    let mut x = 0;
+    let mut y = 0;
+
+    for c in directions.chars() {
+        match c {
+            '^' => y += 1,
+            'v' => y -= 1,
+            '>' => x += 1,
+            '<' => x -= 1,
+            _ => bail!("unexpected character: {c:?}"),
+        }
+
+        visited.insert((x, y));
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let mut even = String::new();
+    let mut odd = String::new();
+    {
+        let (input,) = io::stdin()
+            .lines()
+            .collect_tuple()
+            .context("expected 1 line")?;
+        let input = input?;
+
+        for (i, c) in input.chars().enumerate() {
+            if i % 2 == 0 {
+                even.push(c);
+            } else {
+                odd.push(c);
+            }
+        }
+    }
+
+    let mut visited = HashSet::new();
+    {
+        // Initial house.
+        visited.insert((0, 0));
+        
+        traverse(&even, &mut visited)?;
+        traverse(&odd, &mut visited)?;
+    }
+
+    println!("{}", visited.len());
+
+    Ok(())
+}
+
