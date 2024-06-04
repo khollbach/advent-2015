b8e183ebcf714bebd1199890f7b82dac5c16f5cd
Kevan Hollbach <khollbach@gmail.com>
2024-06-04 16:42:33 -0400

    4-1

---
+++ day-4/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let input = b"iwrupvqb";
+
+    for i in 1.. {
+        let mut concat = vec![];
+        concat.extend_from_slice(input);
+        concat.extend_from_slice(i.to_string().as_bytes());
+
+        let h = md5_hash(&concat);
+        if h.starts_with(&"0".repeat(5)) {
+            dbg!(i);
+            break;
+        }
+    }
+}
+
+fn md5_hash(s: &[u8]) -> String {
+    let digest = md5::compute(s);
+    format!("{:x}", digest)
+}
