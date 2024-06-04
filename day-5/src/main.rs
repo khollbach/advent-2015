5300e12f1276c1ff47560d5dc017d05df6f87b52
Kevan Hollbach <khollbach@gmail.com>
2024-06-04 16:52:12 -0400

    5-2

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use anyhow::Result;
 use itertools::Itertools;
 
-fn main() -> Result<()> {
+fn _main1() -> Result<()> {
     let mut num_nice = 0;
     for l in io::stdin().lines() {
         let l = l?;
@@ -17,4 +17,43 @@ fn main() -> Result<()> {
     }
     dbg!(num_nice);
     Ok(())
+}
+
+fn main() -> Result<()> {
+    let mut num_nice = 0;
+    for l in io::stdin().lines() {
+        let l = l?;
+        let is_nice = first(&l) && second(&l);
+        if is_nice {
+            num_nice += 1;
+        }
+    }
+    dbg!(num_nice);
+    Ok(())
+}
+
+fn first(l: &str) -> bool {
+    assert!(l.is_ascii());
+    let l = l.as_bytes();
+    let n = l.len();
+    for i in 0..=n - 2 {
+        for j in i + 2..=n - 2 {
+            if l[i..][..2] == l[j..][..2] {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn second(l: &str) -> bool {
+    assert!(l.is_ascii());
+    let l = l.as_bytes();
+    let n = l.len();
+    for i in 0..=n - 3 {
+        if l[i] == l[i + 2] {
+            return true;
+        }
+    }
+    false
 }
