a328d467b418fa2de2148333db97ba2e178e3192
Kevan Hollbach <khollbach@gmail.com>
2024-07-06 14:49:57 -0400

    10-1

---
+++ day-10/src/main.rs
@@ -0,0 +1,19 @@
+use itertools::Itertools;
+
+fn main() {
+    let mut s = "1321131112".to_string();
+    for _ in 0..40 {
+        s = next(&s);
+    }
+    dbg!(s.len());
+}
+
+fn next(s: &str) -> String {
+    let mut out = String::new();
+    for (c, chunk) in &s.chars().chunk_by(|&c| c) {
+        let n = chunk.count();
+        out.push_str(&n.to_string());
+        out.push(c);
+    }
+    out
+}
